4763ef693abdcfdd45b8298339f762a4d08bd30e
Madeline Packard <madelinepackard@utexas.edu>
2015-04-14 01:03:48 -0500

    updated RMD

--- 01 SQL Crosstabs/Crosstabs_Project2.R
+++ 01 SQL Crosstabs/Crosstabs_Project2.R
@@ -1,3 +1,11 @@
-toys_df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-+ "select * from GENDER_TOYS "
-+ ')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_map4542', PASS='orcl_map4542', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+toys_df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= "select * from GENDER_TOYS "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_map4542', PASS='orcl_map4542', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+
+#PSAI Rank
+PSAIrank_df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= "select AGE, GENDER, PSAI_TOTAL, rank() 
+OVER (PARTITION BY GENDER order by PSAI_TOTAL asc) as PSAI_RANK from GENDER_TOYS "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_map4542', PASS='orcl_map4542', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+
+#The lower PSAI_Total score, the more female-typical toy preference
+head(PSAIrank_df)
+
--- 01 SQL Crosstabs/Reformatting_Data.R
+++ 01 SQL Crosstabs/Reformatting_Data.R
@@ -8,5 +8,8 @@ df <- read.csv(file_path, stringsAsFactors = FALSE)
 #Replace "." with "_"
 names(df) <- gsub("\\.+", "_", names(df))
 
+#Delete rows with NA data
+df <- na.omit(df)
+
 #Write out reformatted data
 write.csv(df, paste(gsub(".csv", "", file_path), ".reformatted.csv", sep=""), row.names=FALSE)
