c16473d5c30274ece787bff21dbbe7192e12f3c9
Madeline Packard <madelinepackard@utexas.edu>
2015-04-15 11:57:22 -0500

    Updated comments, explained more

--- 01 SQL Crosstabs/Crosstabs_Project2.R
+++ 01 SQL Crosstabs/Crosstabs_Project2.R
@@ -9,18 +9,7 @@ OVER (PARTITION BY GENDER order by PSAI_TOTAL asc) as PSAI_RANK from GENDER_TOYS
 head(PSAIrank_df)
 
 
-#Last_value, max_value, difference !!! DOES NOT WORK !!!
-####
-#PSAIdiff_df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= ""select AGE, PSAI_Total, last_value(max_PSAI_Total) 
-OVER (PARTITION BY AGE order by PSAI_Total) as max_PSAI_Total, last_value(max_PSAI_Total) 
-OVER (PARTITION BY AGE order by PSAI_Total) - PSAI_Total as PSAI_Diff
-from
-(SELECT AGE, PSAI_Total, max(PSAI_Total)
-OVER (PARTITION BY AGE) as max_PSAI_Total 
-FROM GENDER_TOYS) 
-order by 2,3 desc"
-')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_map4542', PASS='orcl_map4542', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
-######
+#Need max_value/last_value/diff dataframe
 
 #nth value
 PSAInthval_df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= "SELECT AGE, GENDER, PSAI_Total, nth_value(PSAI_Total, 2)
