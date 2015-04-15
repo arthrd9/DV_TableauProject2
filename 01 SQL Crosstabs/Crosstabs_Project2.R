0d0fb8ab25e0ac8b1dbf97c2c6aeb950eb258802
Madeline Packard <madelinepackard@utexas.edu>
2015-04-15 13:10:44 -0500

    waiting on diff table

--- 01 SQL Crosstabs/Crosstabs_Project2.R
+++ 01 SQL Crosstabs/Crosstabs_Project2.R
@@ -11,18 +11,15 @@ OVER (PARTITION BY AGE order by PSAI_TOTAL asc) as MASCULINE_BEHAVIOR_RANK from 
 
 #Need max_value/last_value/diff dataframe
 
-#nth value
-PSAInthval_df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= "SELECT AGE, GENDER, PSAI_Total, nth_value(PSAI_Total, 2)
-OVER (PARTITION BY GENDER) as nth_value 
-FROM GENDER_TOYS
-order by 2,3 desc"
-')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_map4542', PASS='orcl_map4542', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+#MAX_PSAI NTH VALUE: Partitioned by AGE and GENDER, and gives the max PSAI_Total score 
+PSAInthval_df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= "SELECT AGE, GENDER, PSAI_Total, nth_value(PSAI_Total, 1)
+OVER (PARTITION BY AGE, GENDER) as MAX_PSAI
+FROM GENDER_TOYS"')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_map4542', PASS='orcl_map4542', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
 #cume_dist
 PSAIcume_dist_df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query= "select AGE, GENDER, PSAI_Total, cume_dist() 
-OVER (PARTITION BY GENDER order by PSAI_Total) as cume_dist
-from GENDER_TOYS
-order by 2,3 asc"
+OVER (PARTITION BY AGE order by PSAI_Total) as cume_dist
+from GENDER_TOYS"
 ')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_map4542', PASS='orcl_map4542', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
 
